c4bd3b092969c5c03aab8f928de572c0bfa1b330
Skjonnhet <Skjonnhet@users.noreply.github.com>
2018-05-04 10:58:24 +0200

    html elements

--- htmlelements/magicCircle.js
+++ htmlelements/magicCircle.js
@@ -1,6 +1,6 @@
 var htmlel_namespace = htmlel_namespace || {};
 
-htmlel_namespace.MAGIC_CIRCLE = {
+htmlel_namespace.MAGIC_CIRCLE = { //Elternelement aller anderen HTML-Elemente
     rootclassname: "magicCircle", //im html-Element class="magicCircle"
     rootid: "magicCircle", //im html-Element id="magicCircle"
     width:200,
@@ -16,3 +16,12 @@ htmlel_namespace.SUN_BURST = {
     height:400,
     type: "g"
 };
+
+htmlel_namepsace.LINE_DIAGRAM = {
+    parentElement: htmlel_namespace.MAGIC_CIRCLE,
+    rootclassname: "linediagram",
+    rootid: "linediagram",
+    width:600,
+    height:400,
+    type: "g"
+}
