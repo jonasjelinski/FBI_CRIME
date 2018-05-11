175d5be45d138f802c258bb2cd4a06654cfe9ab0
jonasjelinski <jonas.jelinski@stud.uni-regensburg.de>
2018-05-11 11:05:30 +0200

    zoom and date on x-axis. next step: clickable labels

--- classes/linechart.js
+++ classes/linechart.js
@@ -44,7 +44,7 @@ class LineChart extends MagicCircle{
 			d3Data.push(newObject);
 			newObject = {};
 		}
-		console.log("d3Data", d3Data);
+		//console.log("d3Data", d3Data);
 
 		return d3Data;
 	}
@@ -61,60 +61,77 @@ class LineChart extends MagicCircle{
         let width = this.width;
 		let labelWidth = width/2;
 		let labelHeight = labelWidth/2;
-		let rootElement = this.rootElement.attr("width", width).attr("height", height);
+		let zooming = d3.zoom()
+		    .scaleExtent([1, Infinity])
+		    .translateExtent([[0, 0], [width, height]])
+		    .extent([[0, 0], [width, height]])
+		    .on("zoom", zoomed);
+		let rootElement = this.rootElement.attr("width", width).attr("height", height).call(zooming);;
 		let maxYear = config_namespace.CONSTANTS.maxYear;
 		let minYear = config_namespace.CONSTANTS.minYear;
 		let minCrime = 0;
 		let maxCrime = 2000;
+		var mindate = new Date(minYear,0,1),
+            maxdate = new Date(maxYear,0,31);
 		
 		let margin = {top: 20, right: 20, bottom: 20, left: 40};
 		let chartWidth = width - margin.left - margin.right;
-		let chartHeight = height - margin.top - margin.bottom;
-		let timeformat = d3.timeParse("%y");
+		let chartHeight = height - margin.top - margin.bottom;		
 		
-		var xRange = d3.scaleTime().domain([minYear, maxYear]).range([0,chartWidth ]); 
+		var xRange = d3.scaleTime().domain([mindate, maxdate]).range([0,chartWidth ]); 
 		var yRange = d3.scaleLinear().domain([minCrime, maxCrime]).range([chartHeight, 0]);
 		
-		var xAxis = d3.axisBottom(xRange).tickFormat(d3.format(".2f"));
-		var yAxis = d3.axisLeft(yRange);
+		var xAxis = d3.axisBottom(xRange); 
+		var yAxis = d3.axisLeft(yRange);	
+
+
+
+			function zoomed() {					
+			    var allLines = rootElement.selectAll(".lines")
+			    	.transition().duration(750)
+			        .attr("transform", d3.event.transform);		
+			        console.log("Al". allLines);	
+			    var line = d3.selectAll('.line').style("stroke-width", 2/d3.event.transform.k);			    
+			    var xCall = xCoord.transition().duration(750).call(xAxis.scale(d3.event.transform.rescaleX(xRange)));
+			    var yCall = yCoord.transition().duration(750).call(yAxis.scale(d3.event.transform.rescaleY(yRange)));
+			  			
+			}							 
+
+
 		
 		var coordinateSystem = rootElement.append("g").attr("class", "coordinateSystem")
 											.attr("width", chartWidth)
-											.attr("height", chartHeight)
+											.attr("height", chartHeight)											
 											.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 							
-			coordinateSystem.append("g").attr("class", "xAxis")
-										.attr("transform", "translate(0,"+chartHeight+")")
-										
-										//.style("fill", "black")
+		var xCoord =	coordinateSystem.append("g").attr("class", "xAxis")
+										.attr("transform", "translate(0,"+chartHeight+")")																	
 										.call(xAxis);	
 
-			coordinateSystem.append("g").attr("class", "yAxis")
-										//.attr("transform","translate(0,-"+margin.bottom+")")	
-										//.style("fill", "black")
+		var yCoord =	coordinateSystem.append("g").attr("class", "yAxis")
+										.attr("transform","translate(0,-"+margin.bottom+")")
 										.call(yAxis);
 
 
 		var simpleline = d3.line()
 					.x(function(d,i){
-						let year = parseInt(d.year);							
-						//console.log("linedata x", d.year); 
-						return xRange(year)})								 
-					.y(function(d,i){
-						//console.log("linedata y", d.crimerate);
+						let year = parseInt(d.year),												
+						date = new Date(year,0,1);						
+						return xRange(date)})								 
+					.y(function(d,i){						
 						let crimerate = parseInt(d.crimerate); 
-						return yRange(crimerate)});								 
+						return yRange(crimerate)});	
 
 
+			
 							
 		var lines = rootElement.append("g").attr("class", "lines")
 								.attr("width", chartWidth)
 								.attr("height", chartHeight)	
-								.attr("transform","translate(" + margin.left + "," + margin.top + ")")
-								//.append("rect").attr("width", chartWidth).attr("height", chartHeight)
+								.attr("transform","translate(" + margin.left + "," + margin.top + ")")								
 								.style("fill", "white");
 			
-			var lineSelection =rootElement.selectAll(".lines");
+		var lineSelection =rootElement.selectAll(".lines");
 			
 
 			lineSelection
@@ -124,16 +141,15 @@ class LineChart extends MagicCircle{
 			.append("path")
 			.attr("class", "line")
 			.attr("d", function(d){
-				let sl = simpleline(d.values);
-				//console.log("sl ",sl);
+				let sl = simpleline(d.values);			
 				return sl;
-			})
-			//.attr("stroke-width", 2)
+			})			
 			.attr("stroke", function(d){
 				let crime = d.key;
 				return commonfunctions_namespace.getCrimeColor(crime);
 			})
 			.attr("fill","none");
+
 
        	}
 	
