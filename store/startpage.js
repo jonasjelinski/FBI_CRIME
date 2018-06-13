dfb3aec5a64f78e1e41790682d83675301209f6f
Unknown <jonas.jelinski@stud.uni-regensburg.de>
2018-06-13 20:33:30 +0200

    Überflüssige Dateien weg und Skriptaufruf in html

--- store/startpage.js
+++ store/startpage.js
@@ -6,23 +6,26 @@ var startpage_namespace = startpage_namespace || {};
 //async scripts 
 //needs to be loaded as soon the page is loaded
 startpage_namespace.loadError = function(oError) {
+	"use strict"
 	throw new URIError("The script " + oError.target.src + " didn't load correctly.");
-}
+};
 
 //Async:False will hold the execution of rest code. Once you get response of ajax, only then, rest of the code will execute.
 startpage_namespace.prefixScript = function(url, onloadFunction) {
+	"use strict"
 	var newScript = document.createElement("script");
 	newScript.onerror = startpage_namespace.loadError;
 	if (onloadFunction) { newScript.onload = onloadFunction; }
 	newScript.async = false;
 	document.currentScript.parentNode.insertBefore(newScript, document.currentScript); 
 	newScript.src = url;
-}
+};
 
 //defer scripts
 //will not run until after the page is loaded
 
 startpage_namespace.loadFirstScripts = function(callback){
+	"use strict"
 	configNamespace.FIRST_SCRIPTS.forEach(function(script){
 		startpage_namespace.prefixScript(script);		
 	});
@@ -30,13 +33,14 @@ startpage_namespace.loadFirstScripts = function(callback){
 	commonfunctionsNamespace.setJsonObject();	
 	commonfunctionsNamespace.setCrimeCorrelationCSV();	
 	callback(null);
-}
+};
 
 startpage_namespace.loadSecondScripts = function(){
+	"use strict"
 	configNamespace.SECOND_SCRIPTS.forEach(function(script){
 		startpage_namespace.prefixScript(script);		
 	});	
-}
+};
 
 startpage_namespace.loadScripts = function(){
 	"use strict"; 
@@ -47,6 +51,5 @@ startpage_namespace.loadScripts = function(){
 		startpage_namespace.loadSecondScripts();
 		console.log("scripts loaded");	
 	}); 
-}
+};
 
-startpage_namespace.loadScripts();
