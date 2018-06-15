38ad6a5011453f2027ffc2f30e2be81847905caa
chris050910 <christian.lisik@gmx.net>
2018-06-15 19:59:33 +0200

    code class timeline was updated
    code timeline was updated

--- classes/timeLine.js
+++ classes/timeLine.js
@@ -1,3 +1,6 @@
+//This class creates a TimeLine with d3
+//An interactive scale bar to chosse the years for the modules "map" and "planets"
+
 class TimeLine extends MagicCircle{
 
 	constructor(pageId){
@@ -8,167 +11,166 @@ class TimeLine extends MagicCircle{
 		this.width = this.htmlelement.width-this.htmlelement.margin.left - this.htmlelement.margin.right;
 		this.height = this.htmlelement.height-this.htmlelement.margin.top - this.htmlelement.margin.bottom;
 		this.yearData=commonfunctionsNamespace.getAllYears();
-    this.eventTarget = new EventTarget();
-    this.onUpdate = "onUpdate";
-    this.timer = 0;
-    this.moving = false;
-    this.update = undefined;
-    this.step = undefined;
+		this.eventTarget = new EventTarget();
+		this.onUpdate = "onUpdate";
+		this.timer = 0;
+		this.currentValue=0;
+		this.startDate = new Date(String(this.yearData[0]));
+		this.endDate = new Date(String(this.yearData[this.yearData.length-1]));
+		this.isMoving = false;
+		this.update = undefined;
+		this.step = undefined;
 	}
 
 	doChart(){
-		if(this.yearData!=undefined){
+		if(this.yearData!==undefined){
 			this.doTimeLine();
 		}
 	}
 
-  isTimeLineMoving(){
-    return this.moving;
-  }
+	isTimeLineMoving(){
+		return this.isMoving;
+	}
 
-   playTimeLine(){
-    console.log("playTimeLine", this.step);
-     this.timer = setInterval(this.step, 100);     
-        this.moving = true;             
-  }
+	playTimeLine(){
+		this.timer = setInterval(this.step, 100);
+		this.isMoving = true;
+	}
 
-   pauseTimeLine(){
-      clearInterval(this.timer);       
-      this.moving = false;
-      this.update();       
-  }
+	pauseTimeLine(){
+		clearInterval(this.timer);
+		this.isMoving = false;
+		this.update();
+	}
 
+	//creates the Timeline
+	//https://gist.github.com/officeofjane/47d2b0bfeecfcb41d2212d06d095c763#file-circles-csv
 	doTimeLine(){
-		var
-			startDate = new Date(String(this.yearData[0])),
-			endDate = new Date(String(this.yearData[this.yearData.length-1])),
-			targetValue = htmlelementsNamespace.TIME_LINE.width,			
+		let startDate = this.startDate,
+			endDate = this.endDate,
+			targetValue = htmlelementsNamespace.TIME_LINE.width,
 			xAxis = scaleTime(startDate, endDate, targetValue),
 			formatDateIntoYear=d3.timeFormat("%Y"),
-			currentValue=0,		
-			timer = 0,
-      that = this;
-     
+			currentValue=this.currentValue,
+			timer = this.timer,
+			that = this,
+			svg = this.container.attr("width", this.width + this.htmlelement.margin.left + this.htmlelement.margin.right).attr("height", this.height+ this.htmlelement.margin.top + this.htmlelement.margin.bottom),
+			g = svg.append(this.htmlElementType).attr("class", "slider").attr("transform", "translate(" + this.htmlelement.margin.left + "," + this.height/5 + ")"),
+			line = drawLine(g, xAxis),
+			handle = drawHandler(g),
+			label = drawLabel(g, startDate);
+		this.update = update;
+		this.step = step;
 
-      let svg = this.container.attr("width", this.width + this.htmlelement.margin.left + this.htmlelement.margin.right).attr("height", this.height+ this.htmlelement.margin.top + this.htmlelement.margin.bottom),
-      g = svg.append(this.htmlElementType).attr("class", "slider").attr("transform", "translate(" + this.htmlelement.margin.left + "," + this.height/5 + ")"),
-      line = drawLine(g, xAxis),
-      handle = drawHandler(g),
-      label = drawLabel(g, startDate);
-      this.update = update;
-      this.step = step; 
+		drawYearOutput(g, xAxis);
+		handlerDragable(line, xAxis, g);
 
-      drawYearOutput(g, xAxis);     
-      handlerDragable(line, xAxis, g); 
+		//show current date above the drag-Handler as text
+		function drawLabel(g,startDate){
+			let label = g.append("text")
+				.attr("class", "label")
+				.attr("text-anchor", "middle")
+				.text(formatDateIntoYear(startDate))
+				.attr("transform", "translate(0," + (-15) + ")");
+			return label;
+		}
 
+		//make drag-Handler dragable
+		function handlerDragable(line,xAxis,g){
+			line.call(d3.drag()
+				.on("start.interrupt", function () { g.interrupt(); })
+				.on("start drag", function() {
+					currentValue = d3.event.x;
+					update();
+				})
+			);
+		}
 
-    function drawLabel(g,startDate){
+		//show drag-Handler as dot
+		function drawHandler(g){
+			let handle = g.insert("circle", ".track-overlay")
+				.attr("class", "handle")
+				.attr("r", 9);
+			return handle;
+		}
 
-      let label = g.append("text")
-                .attr("class", "label")
-                .attr("text-anchor", "middle")
-                .text(formatDateIntoYear(startDate))
-                .attr("transform", "translate(0," + (-15) + ")");
-                return label;
-    }
+		//draw the line and define the beginning -and endRange
+		function drawLine(g,xAxis){
+			let line = g.append("line")
+				.attr("class", "track")
+				.attr("x1", xAxis.range()[0])
+				.attr("x2", xAxis.range()[1])
+				.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+				.attr("class", "track-inset")
+				.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+				.attr("class", "track-overlay");
+			return line;
+		}
 
+		//show the Date-Range below the line
+		function drawYearOutput(g,xAxis){
+			g.insert("g", ".track-overlay")
+				.attr("class", "ticks")
+				.attr("transform", "translate(0," + 16 + ")")
+				.selectAll("text")
+				.data(xAxis.ticks(8))
+				.enter()
+				.append("text")
+				.attr("x", xAxis)
+				.attr("y", 10)
+				.attr("text-anchor", "middle")
+				.text(function(d) { return formatDateIntoYear(d);});
+		}
 
-function handlerDragable(line,xAxis,g){
+		//handle current status of drag-Handler and year-label
+		//send status of year to other modules
+		function update(){
+			let h = xAxis.invert(currentValue),
+				year = formatDateIntoYear(h);
+			sendYearAndMovingStatus(year,that.isMoving);
+			handle.attr("cx", xAxis(h));
+			label
+				.attr("x", xAxis(h))
+				.text(formatDateIntoYear(h));
+		}
 
-  line.call(d3.drag()
-      .on("start.interrupt", function () { g.interrupt(); })
-      .on("start drag", function() {
-                currentValue = d3.event.x;
-                update();
-              })
-          );
-}
+		//shoot the event to let know other modules of year and if drag-Handler is moving
+		function sendYearAndMovingStatus(year,isMoving){
+			let event = new CustomEvent(that.onUpdate, {detail:{ year: year, isMoving: isMoving}});
+			that.eventTarget.dispatchEvent(event);
+		}
 
-    function drawHandler(g){
+		//
+		function step(){
+			update();
+			currentValue = currentValue + (targetValue/99);
+			if (currentValue > targetValue) {
+				this.isMoving = false;
+				update();
+				currentValue = 0;
+			}
+		}
 
-      let handle = g.insert("circle", ".track-overlay")
-                .attr("class", "handle")
-                .attr("r", 9);
-                return handle;
-    }
+		//define the start -and endScale of the line
+		function scaleTime(startDate, endDate, targetValue){
+			let x = d3.scaleTime()
+				.domain([startDate, endDate])
+				.range([0, targetValue])
+				.clamp(true);
+			return x;
+		}
 
-
-    function drawLine(g,xAxis){
-
-      let line = g.append("line")
-              .attr("class", "track")
-              .attr("x1", xAxis.range()[0])
-              .attr("x2", xAxis.range()[1])
-              .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-              .attr("class", "track-inset")
-              .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-              .attr("class", "track-overlay");
-              return line;
-    }
-
-    function drawYearOutput(g,xAxis){
-
-      g.insert("g", ".track-overlay")
-        .attr("class", "ticks")
-        .attr("transform", "translate(0," + 16 + ")")
-        .selectAll("text")
-        .data(xAxis.ticks(8))
-        .enter()
-        .append("text")
-        .attr("x", xAxis)
-        .attr("y", 10)
-        .attr("text-anchor", "middle")
-        .text(function(d) { return formatDateIntoYear(d);});
-    }
-
-    function update() {
-      let h = xAxis.invert(currentValue),
-      year = formatDateIntoYear(h);
-      sendYearAndMovingStatus(year,that.moving);
-
-      handle.attr("cx", xAxis(h));
-        label
-          .attr("x", xAxis(h))
-          .text(formatDateIntoYear(h));
-
-    }
-
-    function sendYearAndMovingStatus(year,moving){
-      let event = new CustomEvent(that.onUpdate, {detail:{ year: year, moving: moving}});      
-      that.eventTarget.dispatchEvent(event);
-    }
-
-    function step() {
-      update();
-      currentValue = currentValue + (targetValue/99);
-      if (currentValue > targetValue) {
-        this.moving = false;
-        update();
-        currentValue = 0;                     
-      }
-    }
-
-    function scaleTime(startDate, endDate, targetValue){
-
-      let x = d3.scaleTime()
-              .domain([startDate, endDate])
-              .range([0, targetValue])
-              .clamp(true);
-              return x;
-    }
-
-    function getLine(x,g,htmlClass){
-
-      let line=g.append("line")
-              .attr(htmlClass, "track")
-              .attr("x1", x.range()[0])
-              .attr("x2", x.range()[1])
-              .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-              .attr(htmlClass, "track-inset")
-              .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-              .attr(htmlClass, "track-overlay");
-              return line;
-    }
-
-  }
+		//getter-function to drawn Line
+		function getLine(x,g,htmlClass){
+			let line=g.append("line")
+				.attr(htmlClass, "track")
+				.attr("x1", x.range()[0])
+				.attr("x2", x.range()[1])
+				.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+				.attr(htmlClass, "track-inset")
+				.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+				.attr(htmlClass, "track-overlay");
+			return line;
+		}
+	}
 }
