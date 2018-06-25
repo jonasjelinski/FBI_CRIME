22bf9cef289377382cc336bdf34dec0f70d98627
Unknown <jonas.jelinski@stud.uni-regensburg.de>
2018-06-25 18:27:39 +0200

    Orderstruktur geändert. Scripts bekommen eigene Ordnerstruktur

---
+++ scripts/scriptLoader.js
@@ -0,0 +1,59 @@
+//loads the js files in the correct order
+//source: https://developer.mozilla.org/en-US/docs/Web/API/HTMLScriptElement#defer_property 
+
+var scriptLoader = scriptLoader || {};
+
+scriptLoader = (function(){
+	"use strict";
+
+	let that = {};
+
+	//async scripts 
+	//needs to be loaded as soon the page is loaded
+	function loadError(oError) {
+		throw new URIError("The script " + oError.target.src + " didn't load correctly.");
+	}
+
+	//Async:False will hold the execution of rest code. Once you get response of ajax, only then, rest of the code will execute.
+	function prefixScript(url, onloadFunction) {
+		var newScript = document.createElement("script");
+		newScript.onerror = loadError;
+		if (onloadFunction) { newScript.onload = onloadFunction; }
+		newScript.async = false;
+		document.currentScript.parentNode.insertBefore(newScript, document.currentScript); 
+		newScript.src = url;
+	}
+
+	//defer scripts
+	//will not run until after the page is loaded
+
+	function loadFirstScripts(callback){
+		scripts_namespace.FIRST_SCRIPTS.forEach(function(script){
+			prefixScript(script);		
+		});
+		commonfunctionsNamespace.setMapJsonObject();
+		commonfunctionsNamespace.setJsonObject();	
+		commonfunctionsNamespace.setCrimeCorrelationCSV();	
+		callback(null);
+	}
+
+	function loadSecondScripts(){
+		scripts_namespace.SECOND_SCRIPTS.forEach(function(script){
+			prefixScript(script);		
+		});	
+	}
+
+	function loadScripts(){
+		var q=d3.queue();
+		q.defer(loadFirstScripts);  
+		q.await(function(error) {
+			if (error) throw error;
+			loadSecondScripts();
+			console.log("scripts loaded");	
+		});
+	}
+
+	that.loadScripts = loadScripts;
+	return that;
+}());
+
