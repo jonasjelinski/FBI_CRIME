8db0db591faec870af1c613c6e19a96a6fc1c1f3
chris050910 <christian.lisik@gmx.net>
2018-07-23 11:29:21 +0200

    better code for class map

--- charts/map.js
+++ charts/map.js
@@ -1,5 +1,10 @@
+//This class creates a Choropleth-Chart
+//it shows which states in the USA have the highest crimerate by color
+//the more red-shades are seen, the higher is the crimerate
+//the more yellow-shades are seen, the lower is the crimerate
+
 class Map extends MagicCircle{
-	constructor(pageId, year=2000, crimeType="Burglary", moving=false){
+	constructor(pageId,year=configNamespace.STATES_AND_CRIMES.minYear,crimeType=configNamespace.STATES_AND_CRIMES.crimeCategories.Crimes.propertyCrime[0],moving){
 		super(pageId);
 		this.htmlelement = htmlelementsNamespace.theMap;
 		this.htmlElementID = this.htmlelement.htmlid;
@@ -10,13 +15,15 @@ class Map extends MagicCircle{
 		this.crime = crimeType;
 		this.moving = moving;
 		this.onClick = "onClick";
+		this.colorRange = 11;
+		this.crimeText = "victims per 100.000 inhabitants";
 		this.eventTarget = new EventTarget();
 	}
 
 	doChart(){
 		let statesData = this.createD3Data(),
 			jsonObject = configNamespace.JSON_OBJECT;
-		if(statesData!==undefined){
+		if(statesData !== undefined){
 			this.doMap(statesData);
 		}
 	}
@@ -45,7 +52,7 @@ class Map extends MagicCircle{
 	createD3Data() {
 		let statesData;
 		if(this.mapData !== false){
-			statesData = topojson.feature(this.mapData, this.mapData.objects.states).features;
+			statesData = topojson.feature(this.mapData,this.mapData.objects.states).features;
 		}
 		return statesData;
 	}
@@ -54,8 +61,8 @@ class Map extends MagicCircle{
 		d3.select(".states").remove();
 		d3.select("#tipBox").remove();
 		let year = this.year,
-			crimeType=this.crime,
-			allStates=commonfunctionsNamespace.getAllStates(),
+			crimeType = this.crime,
+			allStates = commonfunctionsNamespace.getAllStates(),
 			that = this,
 			path = d3.geoPath(d3.geoAlbersUsa()),
 			svg = this.container,
@@ -65,40 +72,54 @@ class Map extends MagicCircle{
 			maxCrime = Math.max.apply(null, allCrimeValues), minCrime = Math.min.apply(null, allCrimeValues),
 			tip = doTip(getAllCrimesNumber);
 
-		prepareStatusSite(getAllCrimesNumber,crimeType,year,0);
-		colorizeMap(g,statesData,path,tip,getAllCrimesNumber,this.moving);
+		prepareStatusSite(getAllCrimesNumber,crimeType,year,0, this.crimeText);
+		colorizeMap(g,statesData,path,tip,getAllCrimesNumber,this.moving,this.colorRange);
 
-		function prepareStatusSite(getAllCrimesNumber,crimeType,year,i){
+		// This function is the first call and prepares the start values, simultaneously it deletes after every call the old values 
+		function prepareStatusSite(getAllCrimesNumber,crimeType,year,i,crimeText){
 			d3.select(".crimeInfo").remove();
 			d3.select(".stateInfo").remove();
 			that.page.append("h2").attr("class","stateInfo").attr("id","stateInfoMapId").text(getAllCrimesNumber[i].state);
-			that.page.append("h2").attr("class","crimeInfo").text(crimeType+': '+getAllCrimesNumber[i].value+' victims per 100.000 inhabitants');
+			that.page.append("h2").attr("class","crimeInfo").text(crimeType+': '+getAllCrimesNumber[i].value+ " "+crimeText);
 		}
 
 		function removeStateInfo(){
 			d3.select(".stateInfo").remove();
 		}
-
-		function colorizeMap(g,statesData,path,tip,getAllCrimesNumber,moving){
-			g.selectAll("path")
+		
+		//Mainfunction: Make sure that the map presents colors for the states
+		function createColorMap(g,statesData,path,getAllCrimesNumber,colorRange){
+			return g.selectAll("path")
 				.data(statesData)
 				.enter().append("path")
 				.attr("d", path)
 				.attr("class", function(d){
-					return fillColorInMap(d);
-				})
-				.call(tip)
+					return fillColorInMap(d,getAllCrimesNumber,colorRange);
+				});
+		}
+		
+		//Send Event to MapPage that a specific state was clicked. Send all informations about the state to "mapPage"
+		function onStateClick(colorMap,tip){
+			return	colorMap.call(tip)
 				.on("click", function(d){
 					removeStateInfo();
 					sendClickEvent(d.properties.name.toUpperCase());
 				})
-				.on('mouseover', function(d){
+		}
+		
+		//Chagne color than the user is on hover on a state
+		function onStateHover(onClick,moving,tip){
+			return onClick.on('mouseover', function(d){
 					if(!moving){
 						tip.show(d);
 						d3.select(this).style("fill", "#ffe9c2").style("cursor", "pointer")
 					}
 				})
-				.on('mouseout', function(d){
+		}
+		
+		//Change color back than the user leaves the hover on a state
+		function onStateLeave(onHover,tip){
+			return onHover.on('mouseout', function(d){
 					tip.hide(d)
 					d3.select(this).style("fill",
 						function(d){
@@ -107,12 +128,20 @@ class Map extends MagicCircle{
 					);
 				});
 		}
+
+		//Call all the functions that needed for colorizig the map
+		function colorizeMap(g,statesData,path,tip,getAllCrimesNumber,moving,colorRange){
+			let colorMap = createColorMap(g, statesData, path, getAllCrimesNumber, colorRange),
+			onClick = onStateClick(colorMap,tip),
+			onHover = onStateHover(onClick,moving,tip),
+			onLeave = onStateLeave(onHover,tip);
+		}
 		
-		function fillColorInMap(d){
+		function fillColorInMap(d,getAllCrimesNumber,colorRange){
 			let styleClass = "state ",
 			quantize = d3.scaleQuantize()
 			.domain([minCrime,maxCrime])
-			.range(d3.range(11).map(function(i){
+			.range(d3.range(colorRange).map(function(i){
 				return "q" + i; }));
 				
 				for(let i=0;i<getAllCrimesNumber.length;i++){
@@ -120,7 +149,7 @@ class Map extends MagicCircle{
 						styleClass+=quantize(parseInt(getAllCrimesNumber[i].value));
 					}
 				}
-				return styleClass;
+			return styleClass;
 		}
 
 		function sendClickEvent(state){
@@ -128,10 +157,13 @@ class Map extends MagicCircle{
 			that.eventTarget.dispatchEvent(event);
 		}
 
+		//function uses the libery "tip" by d3 it registrates the "x" and "y"-position of the mouse. So the System knows on which state 
+		//the user is hovering or clicking a state
 		function doTip(getAllCrimesNumber){
-			var tip = d3.tip()
+			let maxOfSet=10,
+			tip = d3.tip()
 				.offset(function() {
-					return [10,10];
+					return [maxOfSet,maxOfSet];
 				})
 				.html(function(d){
 					return labelStateOnHover(d);
@@ -158,6 +190,8 @@ class Map extends MagicCircle{
 			return allCrimeValues;
 		}
 		
+		//This function is responsible that the states get their crimerates. It is departed into the functions "fillDataInMapViolentCrime" and 
+		// "fillDataInMapPropertyCrime"
 		function getAllCrimesState(allStates,year,crimeType,data){
 			let getAllCrimesNumber=[],
 			crimeValue,
